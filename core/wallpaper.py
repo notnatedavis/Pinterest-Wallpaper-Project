a53cebbd54c4986056ede50fc5a19b47cbcda2aa
ndavispe <157775257+notnatedavis@users.noreply.github.com>
2025-03-14 23:07:21 -0400

    updated file

--- core/wallpaper.py
+++ core/wallpaper.py
@@ -11,17 +11,17 @@ from core.validation import validate_and_preprocess_image
 def set_wallpaper(image_path: str) :
     # sets the given image as the desktop wallpaper based on the OS.
 
-    try:
+    try :
         # validate image and preprocess if necessary
         image_path = validate_and_preprocess_image(image_path)
-        if not image_path:
+        if not image_path :
             logging.error("Image validation failed. Wallpaper not set.")
             return
 
         os_name = platform.system()
 
         # OS selection
-        if os_name == "Windows":
+        if os_name == "Windows" :
             # constants for setting wallpaper
             SPI_SETDESKWALLPAPER = 20  # action
             SPIF_UPDATEINIFILE = 0x01  # update user
@@ -34,7 +34,7 @@ def set_wallpaper(image_path: str) :
             )
             logging.info(f"Wallpaper set to: {image_path} (Windows)")
 
-        elif os_name == "Darwin":  # macOS
+        elif os_name == "Darwin" :  # macOS
             # AppleScript via subprocess
             # TRIPLE CHECK (doesnt work ?)
             script = f"""
@@ -45,8 +45,8 @@ def set_wallpaper(image_path: str) :
             subprocess.run(["osascript", "-e", script], check=True)
             logging.info(f"Wallpaper set to: {image_path} (macOS)")
 
-        else:
+        else :
             logging.warning("Unsupported operating system. Wallpaper not set.")
 
-    except Exception as e:
+    except Exception as e :
         logging.error(f"Error setting wallpaper: {e}")
