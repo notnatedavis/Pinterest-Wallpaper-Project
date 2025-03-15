bb88d2095881052a80d6a724b8c6b08fc4cb1549
ndavispe <157775257+notnatedavis@users.noreply.github.com>
2025-03-14 23:03:57 -0400

    updated file

--- core/downloader.py
+++ core/downloader.py
@@ -2,30 +2,32 @@
 import os
 import requests
 import logging
+from pathlib import Path
+from config import TEMP_FOLDER
 
 # --- other shit here --- #
 
 # --- Functions for Main --- #
-def download_image(image_url: str, save_path: str) -> str :
-    try:
-        # send a GET request to the image URL
+def download_image(image_url: str) -> Path :
+    try :
+        TEMP_FOLDER.mkdir(exist_ok=True)  # ensure folder exists
+        
+        # generate unique filename from URL
+        image_name = "wallpaper.bmp"  # fixed name for wallpaper
+        image_path = TEMP_FOLDER / image_name
+
+        # send a GET request to the image URL , download the image
         response = requests.get(image_url, stream=True)
         response.raise_for_status()  # raise an error if bad responses
 
-        # create a file name from the URL (update in the future for better readability)
-        image_name = image_url.split("/")[-1]
-
-        # full path to save the image
-        image_path = os.path.join(save_path, image_name)
-
-        # write the image to the file (w/ Pillow)
+        # save the image
         with open(image_path, 'wb') as file:
-            for chunk in response.iter_content(1024):  # save in chunks
+            for chunk in response.iter_content(1024):
                 file.write(chunk)
 
-        logging.info(f"Image downloaded and saved to {image_path}")
+        logging.info(f"Image downloaded to {image_path}")
         return image_path
 
     except Exception as e :
-        logging.error(f"Error downloading image: {e}")
-        return ""
+        logging.error(f"Download failed: {e}")
+        return Path("")  # return empty path object on error
