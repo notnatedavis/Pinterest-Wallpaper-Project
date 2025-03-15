d32b7e4a9f5aec477aef590fcbce91ceb5397ec1
ndavispe <157775257+notnatedavis@users.noreply.github.com>
2025-03-14 23:05:56 -0400

    updated file

--- core/pinModel.py
+++ core/pinModel.py
@@ -6,7 +6,7 @@ class Pin :
     def __init__(self, image_url : str) :
         self.image_url = image_url
 
-    def __repr__(self):
+    def __repr__(self) :
         return f"Pin(image_url={self.image_url})"
 
 class Board :
@@ -15,11 +15,12 @@ class Board :
         self.url = url
         self.pins = []
 
-    def add_pin(self, pin: Pin):
+    def add_pin(self, pin: Pin) :
         self.pins.append(pin)
-        logging.info(f"Added Pin to Board '{self.name}': {pin}")
+        # logging.info(f"added pin to board")
+        # logging.info(f"Added Pin to Board '{self.name}': {pin}")
     
-    def __repr__(self):
+    def __repr__(self) :
         return f"Board(name={self.name}, url={self.url}, pins={len(self.pins)})"
 
 # --- other shit here --- #
