834b813e705732fb203bc855f1fc46e62119bafa
ndavispe <157775257+notnatedavis@users.noreply.github.com>
2025-03-14 23:08:42 -0400

    updated file

--- config.py
+++ config.py
@@ -3,21 +3,20 @@ import os
 from pathlib import Path
 
 # --- Universal Paths --- #
-# Get the root directory of project (where config.py is located)
+# get the root directory of project (where config.py is located)
 PROJECT_ROOT = Path(__file__).resolve().parent
 
-# Define paths relative to the project root
+# define paths relative to the project root
 PRESET_SOURCE = PROJECT_ROOT  # Use the project root as the source directory
 PRESET_DESTINATION = PROJECT_ROOT  # Use the project root as the destination directory
 
-# Temporary folder for downloaded images
-TEMP_FOLDER = os.path.join(PROJECT_ROOT, "temp_images")
+TEMP_FOLDER = PROJECT_ROOT / "temp_images"
 
-# Ensure the temp folder exists
-os.makedirs(TEMP_FOLDER, exist_ok=True)
+# ensure the temp folder exists
+# TEMP_FOLDER.mkdir(parents=True, exist_ok=True)
 
-# Specific variables
-SCROLL_LIMIT = 3 # remove ?
-SLEEP_TIME = 3 # update ?
+# specific variables
+SCROLL_LIMIT = 20 # remove ?
+SLEEP_TIME = 1.5 # update ?
 
 # --- other shit here --- #
